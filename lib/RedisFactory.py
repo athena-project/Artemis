ac899b7e55309c754949046312bf7c76192988b7
PROSPERI Laurent <oceane21.sk1@live.fr>
2015-05-10 18:20:47 +0200

    2.0.0-a

--- lib/RedisFactory.py
+++
@@ -1,4 +0,0 @@
-import redis
-
-def getConn():
-	return redis.StrictRedis(host='92.222.72.175', port=6379, db=0)
--- lib/SQLFactory.py
+++
@@ -1,5 +0,0 @@
-import pymysql
-
-def getConn():
-	return pymysql.connect(database='artemis', host='92.222.72.175', user='root',
-							passwd='rj7@kAv;8d7_e(E6:m4-w&', connect_timeout=200)
